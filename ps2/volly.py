6a9d5ea66ef5fe4f79809672b7b453dd744b0adf
Vishal Ravikumar <vr36092002@gmail.com>
2024-10-04 10:06:15 -0400

    added user input as well as storage.

--- ps2/volly.py
+++ ps2/volly.py
@@ -1,1 +1,18 @@
-# THis is a program that that will accept the number of points that each team scored in the 5 matches and print out thedata along with the winne
+# THis is a program that that will accept the number of points that each team scored in the 5 matches and print out the data along with the winne
+
+team1 = []
+team2 = []
+
+for i in range (5):
+    userInput1= input("Enter the score that team one got on match 1: ")
+    userInput2= input("Enter the score that team one got on match 2: ")
+    team1.append(userInput1)
+    team2.append(userInput2)
+
+
+#Create a var called temp that stores the number when its being checked, loop through the array and then compare it to the second array and if its greater store it in temp if not keep going. a
+
+
+
+print(team1)
+print(team2)
